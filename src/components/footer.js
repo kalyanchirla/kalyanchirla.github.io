4a3a0556c684f6f8e9bb96f5c9401db730c9d4ae
Kalyan Chirla <kalyanchirla@Kalyans-MacBook-Pro.local>
2021-11-05 23:47:18 +0000

    Updated Files

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/kalyanchirla/portfolio"
+          href="https://github.com/kalyanchirla/kalyanchirla.github.io"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>
